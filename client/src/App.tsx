696853c767dfb62c93cc44165b992e96018c02c8
Alex62510 <eagle625@tut.by>
2024-02-09 13:53:48 +0300

    create routing pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import {About} from "./pages/About.tsx";
+import {SignIn} from "./pages/SignIn.tsx";
+import {SignUp} from "./pages/SignUp.tsx";
+import {Dashboard} from "./pages/Dashboard.tsx";
+import {Projects} from "./pages/Projects.tsx";
 
 
 export const App = () => {
     return (
-        <div className={'text-3xl'}>
-app
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/sign-in'} element={<SignIn/>}/>
+                <Route path={'/about'} element={<About/>}/>
+                <Route path={'/sign-up'} element={<SignUp/>}/>
+                <Route path={'/dashboard'} element={<Dashboard/>}/>
+                <Route path={'/projects'} element={<Projects/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
