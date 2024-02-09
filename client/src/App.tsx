0ad28264dc16c98078999fe351ba538c24369e98
Alex62510 <eagle625@tut.by>
2024-02-09 14:11:42 +0300

    add lint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import {About} from "./pages/About.tsx";
-import {SignIn} from "./pages/SignIn.tsx";
-import {SignUp} from "./pages/SignUp.tsx";
-import {Dashboard} from "./pages/Dashboard.tsx";
-import {Projects} from "./pages/Projects.tsx";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import { About } from './pages/About';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
+import { Dashboard } from './pages/Dashboard';
+import { Projects } from './pages/Projects';
 
 export const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={'/'} element={<Home/>}/>
-                <Route path={'/sign-in'} element={<SignIn/>}/>
-                <Route path={'/about'} element={<About/>}/>
-                <Route path={'/sign-up'} element={<SignUp/>}/>
-                <Route path={'/dashboard'} element={<Dashboard/>}/>
-                <Route path={'/projects'} element={<Projects/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/sign-in'} element={<SignIn />} />
+        <Route path={'/about'} element={<About />} />
+        <Route path={'/sign-up'} element={<SignUp />} />
+        <Route path={'/dashboard'} element={<Dashboard />} />
+        <Route path={'/projects'} element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
-
