ad4532631df760a0c268fd72cee1f3ef2f04e3f3
Alex62510 <eagle625@tut.by>
2024-02-13 22:18:52 +0300

    add user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://kartinki.pics/uploads/posts/2022-12/thumbs/1670577804_5-kartinkin-net-p-neitralnie-kartinki-vkontakte-5.png"
+    }
+}, {timestamps: true})
+const User = mongoose.model('User', userSchema)
+export default User
