3ce9b8bbbc41e2330e182f865ab9df392521df96
Alex62510 <eagle625@tut.by>
2024-02-21 21:47:37 +0300

    add google auth

--- client/src/components/Oauth.tsx
+++ client/src/components/Oauth.tsx
@@ -3,15 +3,36 @@ import { Button } from 'flowbite-react';
 import { AiFillGoogleCircle } from 'react-icons/ai';
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
 import { app } from '../firebase';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { AppDispatch } from '../redux/store';
+import { useDispatch } from 'react-redux';
 
 export const Oauth = () => {
   const auth = getAuth(app);
+  const navigate = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
   const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: 'select_account' });
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
-      console.log(resultsFromGoogle);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          photo: resultsFromGoogle.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
     } catch (e) {
       console.log(e);
     }
