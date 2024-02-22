b034549dd9cec9c5bde2b4fdc0950f237fbd0727
Alex62510 <eagle625@tut.by>
2024-02-22 22:59:01 +0300

    add theme

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Button, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { RootState } from '../redux/store';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { AppDispatch, RootState } from '../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
 import { Profile } from './Profile';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export const Header = () => {
   const path = useLocation().pathname;
-  const { error, currentUser } = useSelector((state: RootState) => state.user);
+  const dispatch: AppDispatch = useDispatch();
+  const { theme } = useSelector((state: RootState) => state.theme);
+  const { currentUser } = useSelector((state: RootState) => state.user);
   return (
     <Navbar className={'border-b-2'}>
       <Link
@@ -39,18 +42,25 @@ export const Header = () => {
         <AiOutlineSearch />
       </Button>
       <div className={'flex gap-2 md:order-2'}>
-        <Button className={'w-12 h-10 hidden sm:inline'} color="green" pill>
-          <FaMoon />
+        <Button
+          className={'w-12 h-10 hidden sm:inline'}
+          color="green"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        <Link to={'/sign-in'}>
-          {currentUser ? (
-            <Profile />
-          ) : (
+
+        {currentUser ? (
+          <Profile />
+        ) : (
+          <Link to={'/sign-in'}>
             <Button gradientDuoTone={'greenToBlue'} outline className={'transition'}>
               Sign in
             </Button>
-          )}
-        </Link>
+          </Link>
+        )}
+
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
