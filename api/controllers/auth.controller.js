1e8b5ac98a43c3f51146d5578ad06a849ded60eb
Alex62510 <eagle625@tut.by>
2024-02-29 22:00:11 +0300

    add update user logic in UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,12 +8,12 @@ export const signup = async (req, res, next) => {
     if (!username || !email || !password || username === '' || email === '' || password === '') {
         next(errorHandler(400, "All field are required"))
     }
-    const hashedPassword = bcryptjs.hashSync(password, 12)
+    const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({username, email, password: hashedPassword})
 
     try {
         await newUser.save()
-        res.json("Signup successful")
+        res.status(201).json("Signup successful")
     } catch (e) {
         next(e)
     }
@@ -38,7 +38,7 @@ export const signin = async (req, res, next) => {
         }, process.env.JWT_SECRET)
 
         const {password: pass, ...rest} = validUser._doc
-        res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest)
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
     } catch (e) {
         next(e)
     }
---
+++ client/src/types/types.ts
@@ -0,0 +1,6 @@
+export interface FormData {
+  profilePicture?: string;
+  username: string;
+  email: string;
+  password: string;
+}
