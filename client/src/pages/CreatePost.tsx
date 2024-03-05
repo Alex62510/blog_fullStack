64c262b419d34bda645c20fa056e8682bb3c3989
Alex62510 <eagle625@tut.by>
2024-03-05 21:26:39 +0300

    create post page submit

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Alert, Button, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -6,16 +6,22 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 type FormDataType = {
   image?: string;
+  title: string;
+  content: string;
+  category: string;
 };
 
 export const CreatePost = () => {
   const [file, setFile] = useState<File | null>(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(0);
   const [imageUploadError, setImageUploadError] = useState('');
+  const [publishError, setPublishError] = useState('');
   const [formData, setFormData] = useState<FormDataType>({});
+  const navigate = useNavigate();
 
   const handleUploadImage = async () => {
     try {
@@ -59,11 +65,33 @@ export const CreatePost = () => {
       setFile(files[0]);
     }
   };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      setImageUploadError('');
+      navigate(`/post/${data.slug}`);
+    } catch (e) {
+      setPublishError('Something went wrong');
+    }
+  };
 
   return (
     <div className={'p-3 max-w-3xl mx-auto min-h-screen'}>
       <h1 className={'text-center text-3xl my-7 font-semibold'}>Create a post</h1>
-      <form className={'flex flex-col gap-4'}>
+      <form className={'flex flex-col gap-4'} onSubmit={handleSubmit}>
         <div className={'flex flex-col gap-4 sm:flex-row justify-between'}>
           <TextInput
             type={'text'}
@@ -71,8 +99,9 @@ export const CreatePost = () => {
             required
             id={'title'}
             className={'flex-1'}
+            onChange={e => setFormData({ ...formData, title: e.target.value })}
           />
-          <Select>
+          <Select onChange={e => setFormData({ ...formData, category: e.target.value })}>
             <option value={'uncategorized'}>Select a category</option>
             <option value={'typescript'}>typeScript</option>
             <option value={'reactjs'}>React.js</option>
@@ -119,6 +148,7 @@ export const CreatePost = () => {
           />
         )}
         <ReactQuill
+          onChange={value => setFormData({ ...formData, content: value })}
           theme={'snow'}
           placeholder={'Write something...'}
           className={`h-72 mb-12 dark:text-cyan-100 dark:placeholder:text-cyan-100 `}
@@ -126,6 +156,11 @@ export const CreatePost = () => {
         <Button type={'submit'} gradientDuoTone={'purpleToBlue'}>
           Publish
         </Button>
+        {publishError && (
+          <Alert className={'mt-5'} color={'failure'}>
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
   );
