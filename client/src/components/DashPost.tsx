d7753446f24066d1e798d9fa4b402e7ff30e35ad
Alex62510 <eagle625@tut.by>
2024-03-07 18:20:00 +0300

    add posts to page

--- client/src/components/DashPost.tsx
+++ client/src/components/DashPost.tsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import { Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+
+type PostType = {
+  category: string;
+  content: string;
+  createdAt: string;
+  image: string;
+  slug: string;
+  title: string;
+  updatedAt: string;
+  userId: string;
+  __v: string;
+  _id: string;
+};
 
 export const DashPost = () => {
   const { currentUser, loading, error } = useSelector((state: RootState) => state.user);
-  const [userPosts, setUserPosts] = useState([]);
+  const [userPosts, setUserPosts] = useState<PostType[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -16,10 +31,93 @@ export const DashPost = () => {
             setUserPosts(data.posts);
           }
         }
-      } catch (e) {}
+      } catch (e) {
+        console.log(e);
+      }
     };
-    fetchPosts();
-  }, [currentUser, currentUser._id]);
+    if (currentUser && currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser?._id]);
+
   console.log(userPosts);
-  return <div>DashPost</div>;
+  return (
+    <div
+      className={
+        'table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-teal-100 ' +
+        'scrollbar-thumb-teal-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'
+      }
+    >
+      {currentUser && currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className={'shadow-md'}>
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span className={''}>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map(post => (
+              <Table.Body className={'divide-y'}>
+                <Table.Row
+                  className={
+                    'bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-neutral-200 hover:shadow-lg hover:animate-pulse'
+                  }
+                >
+                  <Table.Cell className={'dark:text-teal-100'}>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className={'w-20 h-10 object-cover bg-gray-500 '}
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className={'font-medium text-green-900 dark:text-teal-100'}
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell className={'dark:text-teal-100'}>
+                    {post.category}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <span
+                      className={
+                        'font-medium text-red-500 cursor-pointer hover:underline transition hover:text-red-900 hover:font-semibold'
+                      }
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className={
+                        'text-teal-300 hover:underline transition hover:text-teal-950'
+                      }
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+    </div>
+  );
 };
