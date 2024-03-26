181ca69b84ca92c261359a5061cdb4b66d94a075
Alex62510 <eagle625@tut.by>
2024-03-26 21:07:19 +0300

    create Dashsidebar  dashboard link

--- client/src/components/DashSidebar.tsx
+++ client/src/components/DashSidebar.tsx
@@ -6,6 +6,7 @@ import {
   HiDocumentText,
   HiOutlineUserGroup,
   HiAnnotation,
+  HiChartPie,
 } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { signOut } from '../redux/user/userSlice';
@@ -46,6 +47,19 @@ export const DashSidebar = () => {
     <Sidebar className={'w-full md:w-56'}>
       <Sidebar.Items>
         <Sidebar.ItemGroup className={'flex flex-col gap-1 '}>
+          {currentUser && currentUser.isAdmin && (
+            <Link to={'/dashboard?tab=dash'}>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                className={'hover:bg-neutral-300 hover:shadow-lg hover:animate-pulse'}
+                icon={HiChartPie}
+                labelColor={'dark'}
+                as={'div'}
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to={'/dashboard?tab=profile'}>
             <Sidebar.Item
               className={'hover:bg-neutral-300 hover:shadow-lg hover:animate-pulse'}
